ef8149407bb0e2a221fd755893e5e02ee06a0371
baolongnguyenmac <baolongnguyen.mac@gmail.com>
2025-09-02 15:49:44 +0700

    lib + clean the code

---
+++ clean.sh
@@ -0,0 +1,13 @@
+rm -rf */*.aux \
+    */*.bbl \
+    */*.blg \
+    */*.dvi \
+    */*.fdb_latexmk \
+    */*.log \
+    */*.nav \
+    */*.out \
+    */*.snm \
+    */*.synctex.gz \
+    */*.toc \
+    */*.fls \
+    */*.xdv
